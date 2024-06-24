05ed864d90590419241269ff6eaad96c5831ee75
Jake Wang <jakew4@illinois.edu>
2024-06-23 21:28:00 -0500

    create two panels

---
+++ components/LeftPanel.tsx
@@ -0,0 +1,15 @@
+export const LeftPanel = () => {
+  return (
+    <div className="text-white flex flex-col gap-4">
+      <div className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+        &quot;Jake&quot; Zhi Wang
+      </div>
+      <div className="text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+        Full Stack Software Engineer
+      </div>
+      <p className="text-slate-400 pr-56">
+        I build pixel-perfect, engaging, and accessible digital experiences.
+      </p>
+    </div>
+  );
+};
---
+++ components/RightPanel.tsx
@@ -0,0 +1,22 @@
+export const RightPanel = () => {
+  return (
+    <div>
+      <div className="text-slate-400 text-base flex flex-col gap-4">
+        <p>
+          Motivated software engineer with 2+ years of front-end and back-end
+          development experience, currently pursuing a master&apos;s degree in
+          Computer Science at the University of Illinois-Urbana-Champaign.
+        </p>
+        <p>
+          Proficient in TypeScript/JavaScript, Java, Python and C++, and skilled
+          in using Visual Studio, IntelliJ, and Eclipse development tools.
+          Experienced in program development, optimization, refactoring, and
+          testing.
+        </p>
+        <p>
+          Adaptable and eager to learn new technologies and coding languages.
+        </p>
+      </div>
+    </div>
+  );
+};
