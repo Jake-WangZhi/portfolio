3486a089bdc6042b328d3eb375df4feca9b59aa8
Jake Wang <jakew4@illinois.edu>
2024-07-01 20:40:55 -0500

    change the hover cover effect

--- components/ExpCard.tsx
+++ components/ExpCard.tsx
@@ -1,4 +1,4 @@
-import { Chip, Grid, Paper } from "@mui/material";
+import { Chip, Grid } from "@mui/material";
 import { useState } from "react";
 import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
@@ -22,63 +22,58 @@ export default function ExpCard({
   const [hover, setHover] = useState(false);
 
   return (
-    <a href={url} target="_blank">
-      <Paper
-        sx={{
-          backgroundColor: "transparent",
-          "&:hover": {
-            backgroundColor: "rgba(128, 128, 128, 0.15)",
-          },
-        }}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-        <div className="text-slate-400 p-2">
-          <Grid container spacing={0}>
-            <Grid item xs={12} md={3}>
-              <div className="text-sm pt-0.5">{time}</div>
-            </Grid>
-            <Grid item xs={12} md={9}>
-              <div className="flex flex-col gap-2">
-                <h1
-                  className={`text-lg font-medium ${
-                    hover ? "text-teal-300 underline" : "text-slate-200"
-                  }`}
-                >
-                  {title}
-                </h1>
-                <div
-                  className={`flex text-sm justify-end gap-1 items-center ${
-                    hover ? "text-teal-300" : "text-slate-200"
-                  }`}
-                >
-                  {company}
-                  <ArrowOutwardIcon style={{ fontSize: 20 }} />
-                </div>
-                <ul className="list-disc pl-5">
-                  {description.map((desc, index) => (
-                    <li key={index} className="tracking-tight">
-                      {desc}
-                    </li>
-                  ))}
-                </ul>
-                <div className="flex gap-2 pt-2 flex-wrap">
-                  {techStacks.map((tech, index) => (
-                    <Chip
-                      key={index}
-                      label={tech}
-                      className="bg-teal-400/10 text-teal-300 text-xs font-normal leading-5"
-                      style={{
-                        fontFamily: "inherit",
-                      }}
-                    />
-                  ))}
-                </div>
-              </div>
-            </Grid>
+    <a
+      href={url}
+      target="_blank"
+      className="rounded-md bg-transparent hover:bg-slate-800/50 hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className="text-slate-400 p-2">
+        <Grid container spacing={0}>
+          <Grid item xs={12} md={3}>
+            <div className="text-sm pt-0.5">{time}</div>
           </Grid>
-        </div>
-      </Paper>
+          <Grid item xs={12} md={9}>
+            <div className="flex flex-col gap-2">
+              <h1
+                className={`text-lg font-medium ${
+                  hover ? "text-teal-300 underline" : "text-slate-200"
+                }`}
+              >
+                {title}
+              </h1>
+              <div
+                className={`flex text-sm justify-end gap-1 items-center ${
+                  hover ? "text-teal-300" : "text-slate-200"
+                }`}
+              >
+                {company}
+                <ArrowOutwardIcon style={{ fontSize: 20 }} />
+              </div>
+              <ul className="list-disc pl-5">
+                {description.map((desc, index) => (
+                  <li key={index} className="tracking-tight">
+                    {desc}
+                  </li>
+                ))}
+              </ul>
+              <div className="flex gap-2 pt-2 flex-wrap">
+                {techStacks.map((tech, index) => (
+                  <Chip
+                    key={index}
+                    label={tech}
+                    className="bg-teal-400/10 text-teal-300 text-xs font-normal leading-5"
+                    style={{
+                      fontFamily: "inherit",
+                    }}
+                  />
+                ))}
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
     </a>
   );
 }
