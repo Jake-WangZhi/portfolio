c50291e350cbd532eafc8fd3e6cdfb20ef6f0e86
Jake Wang <jakewangzhi@gmail.com>
2024-07-24 17:00:45 -0500

    make the section name personal projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -104,7 +104,7 @@ export const Projects = () => {
   return (
     <div className="flex flex-col gap-4 pt-4">
       <div className="text-lg font-medium tracking-tight text-slate-200 sm:text-2xl max-sm:py-4 max-sm:z-50 max-sm:sticky max-sm:top-0 max-sm:bg-background">
-        PROJECTS
+        PERSONAL PROJECTS
       </div>
       {PROJECTS.map((project, index) => (
         <ProjectCard
